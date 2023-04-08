999bb54a79a531131756e4b474da290f534c8a4b
Ivan Lemovou <lemovou@gmail.com>
2023-04-08 08:27:15 +0100

    added env variables

---
+++ lib/env/env.dart
@@ -0,0 +1,24 @@
+import 'package:envied/envied.dart';
+
+part 'env.g.dart';
+
+@Envied(path: '.env')
+abstract class Env {
+  @EnviedField(varName: 'API_URL', obfuscate: true)
+  static final apiUrl = _Env.apiUrl;
+
+  @EnviedField(varName: 'API_KEY', obfuscate: true)
+  static final apiKey = _Env.apiKey;
+
+  @EnviedField(varName: 'CLIENT_ID', obfuscate: true)
+  static final clientId = _Env.clientId;
+
+  @EnviedField(varName: 'APP_VERSION', obfuscate: true)
+  static final appVersion = _Env.appVersion;
+
+  @EnviedField(varName: 'APP_PLATFORM', obfuscate: true)
+  static final appPlatform = _Env.appPlatform;
+
+  @EnviedField(varName: 'CLIENT', obfuscate: true)
+  static final appClient = _Env.appClient;
+}
--- lib/network/interceptor.dart
+++ lib/network/interceptor.dart
@@ -1,152 +1,92 @@
-// import 'package:dio/dio.dart';
-//
-//
-// class TokenInterceptor extends QueuedInterceptor {
-//   String? accessToken;
-//
-//   Future<SharedPreferences> instance = SharedPreferences.getInstance();
-//
-//   @override
-//   void onRequest(
-//       RequestOptions options, RequestInterceptorHandler handler) async {
-//     if (accessToken == null) {
-//       accessToken = await SharedPreferenceHelper(instance).authToken;
-//
-//       if (accessToken == null) {
-//         return handler.next(options);
-//       }
-//     }
-//
-//     if (options.path != "/auth/refresh-tokens") {
-//       options.headers["Authorization"] = "Bearer $accessToken";
-//     }
-//     return handler.next(options);
-//   }
-//
-//   @override
-//   Future<void> onError(DioError err, ErrorInterceptorHandler handler) async {
-//     if (err.type == DioErrorType.response &&
-//             err.response?.statusCode == 403 ||
-//         err.response?.statusCode == 401) {
-//       var options = err.response?.requestOptions;
-//
-//       if (options?.data is FormData) {
-//         FormData formData = FormData();
-//         formData.fields.addAll(options?.data.fields);
-//         for (MapEntry mapFile in options?.data.files) {
-//           /*  formData.files.add(MapEntry(mapFile.key,
-//               MultipartFileExtended.fromFileSync(mapFile.value.filePath)));*/
-//         }
-//         options?.data = formData;
-//       }
-//       // If the token has been updated, repeat directly.
-//       if (options?.headers['Authorization'] != null &&
-//           !options!.headers['Authorization']
-//               .toString()
-//               .contains(accessToken!)) {
-//         options.headers["Authorization"] = "Bearer ${accessToken!}";
-//         //repeat
-//         Dio().fetch(options).then(
-//           (r) => handler.resolve(r),
-//           onError: (e) {
-//             handler.reject(e);
-//           },
-//         );
-//         return;
-//       }
-//
-//       Dio refreshDio = Dio(BaseOptions(baseUrl: Configs.apiUrl));
-//       var refreshToken = await SharedPreferenceHelper(instance).refreshToken;
-//       Map<String, dynamic> data = {
-//         "refreshToken": refreshToken,
-//       };
-//
-//       // Add the Api method to get the token on the server
-//       refreshDio.post("/auth/refresh-tokens", data: data).then((value) async {
-//         // need to modify this
-//         accessToken = value.data["access"]["token"];
-//         await SharedPreferenceHelper(instance).saveAuthToken(accessToken!);
-//         await SharedPreferenceHelper(instance)
-//             .saveRefreshToken(value.data["refresh"]["token"]);
-//         options?.headers["Authorization"] = "Bearer ${accessToken!}";
-//
-//         //repeat
-//         Dio().fetch(options!).then(
-//           (r) => handler.resolve(r),
-//           onError: (e) {
-//             handler.reject(e);
-//           },
-//         );
-//       }).onError((error, stackTrace) {
-//         handler.next(error as DioError);
-//       });
-//
-//       return;
-//     }
-//
-//     return handler.next(err);
-//   }
-// }
-//
-// class EntityTokenInterceptor extends QueuedInterceptor {
-//   Future<SharedPreferences> instance = SharedPreferences.getInstance();
-//
-//   @override
-//   void onRequest(
-//       RequestOptions options, RequestInterceptorHandler handler) async {
-//     String? accessToken = await SharedPreferenceHelper(instance).entityToken;
-//     if (accessToken == null) {
-//       return handler.next(options);
-//     }
-//     if (!options.path.contains("/auth")) {
-//       options.queryParameters["token"] = accessToken;
-//     }
-//
-//     return handler.next(options);
-//   }
-//
-//   @override
-//   Future<void> onError(DioError error, ErrorInterceptorHandler handler) async {
-//     if (error.type == DioErrorType.response &&
-//             error.response?.statusCode == 403 ||
-//         error.response?.statusCode == 401) {
-//       var options = error.response?.requestOptions;
-//
-//       if (options?.data is FormData) {
-//         FormData formData = FormData();
-//         formData.fields.addAll(options?.data.fields);
-//         for (MapEntry mapFile in options?.data.files) {
-//           /*  formData.files.add(MapEntry(mapFile.key,
-//               MultipartFileExtended.fromFileSync(mapFile.value.filePath)));*/
-//         }
-//         options?.data = formData;
-//       }
-//
-//       Dio refreshDio = Dio(BaseOptions(baseUrl: Configs.basketUrl));
-//       // Add the Api method to get the token on the server
-//       refreshDio
-//           .post(
-//               "/auth?access_key=${Configs.basketAccessKey}&secret_key=${Configs.basketSecretKey}")
-//           .then((value) async {
-//         // need to modify this
-//         await SharedPreferenceHelper(instance)
-//             .saveEntitySportToken(value.data["response"]["token"]);
-//         options?.queryParameters["token"] = value.data["response"]["token"];
-//
-//         //repeat
-//         Dio().fetch(options!).then(
-//           (r) => handler.resolve(r),
-//           onError: (e) {
-//             handler.reject(e);
-//           },
-//         );
-//       }).onError((error, stackTrace) {
-//         handler.next(error as DioError);
-//       });
-//
-//       return;
-//     }
-//
-//     return handler.next(error);
-//   }
-// }
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../env/env.dart';
+import '../helper/shared_preference_helper.dart';
+
+class TokenInterceptor extends QueuedInterceptor {
+  String? accessToken;
+
+  Future<SharedPreferences> instance = SharedPreferences.getInstance();
+
+  @override
+  void onRequest(
+      RequestOptions options, RequestInterceptorHandler handler) async {
+    if (accessToken == null) {
+      accessToken = await SharedPreferenceHelper(instance).authToken;
+
+      if (accessToken == null) {
+        return handler.next(options);
+      }
+    }
+
+    if (options.path != "/auth/refresh-tokens") {
+      options.headers["Authorization"] = "Bearer $accessToken";
+    }
+    return handler.next(options);
+  }
+
+  @override
+  Future<void> onError(DioError err, ErrorInterceptorHandler handler) async {
+    if (err.type == DioErrorType.badResponse &&
+            err.response?.statusCode == 403 ||
+        err.response?.statusCode == 401) {
+      var options = err.response?.requestOptions;
+
+      if (options?.data is FormData) {
+        FormData formData = FormData();
+        formData.fields.addAll(options?.data.fields);
+        for (MapEntry mapFile in options?.data.files) {
+          /*  formData.files.add(MapEntry(mapFile.key,
+              MultipartFileExtended.fromFileSync(mapFile.value.filePath)));*/
+        }
+        options?.data = formData;
+      }
+      // If the token has been updated, repeat directly.
+      if (options?.headers['Authorization'] != null &&
+          !options!.headers['Authorization']
+              .toString()
+              .contains(accessToken!)) {
+        options.headers["Authorization"] = "Bearer ${accessToken!}";
+        //repeat
+        Dio().fetch(options).then(
+          (r) => handler.resolve(r),
+          onError: (e) {
+            handler.reject(e);
+          },
+        );
+        return;
+      }
+
+      Dio refreshDio = Dio(BaseOptions(baseUrl: Env.apiUrl));
+      var refreshToken = await SharedPreferenceHelper(instance).authToken;
+      Map<String, dynamic> data = {
+        "refreshToken": refreshToken,
+      };
+
+      // Add the Api method to get the token on the server
+      refreshDio.post("/auth/refresh-tokens", data: data).then((value) async {
+        // need to modify this
+        accessToken = value.data["access"]["token"];
+        await SharedPreferenceHelper(instance).saveAuthToken(accessToken!);
+        await SharedPreferenceHelper(instance)
+            .saveAuthToken(value.data["refresh"]["token"]);
+        options?.headers["Authorization"] = "Bearer ${accessToken!}";
+
+        //repeat
+        Dio().fetch(options!).then(
+          (r) => handler.resolve(r),
+          onError: (e) {
+            handler.reject(e);
+          },
+        );
+      }).onError((error, stackTrace) {
+        handler.next(error as DioError);
+      });
+
+      return;
+    }
+
+    return handler.next(err);
+  }
+}
